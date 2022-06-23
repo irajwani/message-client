e7a5e1c4ff923a389e557d09a70d49c85182ae2e
Imad Rajwani <imadrajwani@Imads-MacBook-Pro-2.local>
2022-06-23 21:25:48 +0300

    i add username field

--- chat-socket.js
+++ chat-socket.js
@@ -1,27 +1,31 @@
-const token = document.getElementById('token');
-const room = document.getElementById('room');
+const token = document.getElementById("token");
+const room = document.getElementById("room");
 
-const socket = io(`http://localhost:3000?token=${token.value}&room=${room.value}`, { transport : ['websocket'] })
+const socket = io(
+  `http://localhost:3000?token=${token.value}&room=${room.value}`,
+  { transport: ["websocket"] }
+);
 
-const message = document.getElementById('message');
-const messages = document.getElementById('messages');
+const username = document.getElementById("username");
+const message = document.getElementById("message");
+const messages = document.getElementById("messages");
 
 const handleSubmitNewMessage = () => {
-    socket.emit('message', { text: message.value })
-}
+  socket.emit("message", { text: message.value, recipient: username.value });
+};
 
-socket.on('message', (messages) => {
-    messages.forEach(message => {
-        handleNewMessage(message);
-    });
-})
+socket.on("message", (messages) => {
+  messages.forEach((message) => {
+    handleNewMessage(message);
+  });
+});
 
 const handleNewMessage = (message) => {
-    messages.appendChild(buildNewMessage(message));
-}
+  messages.appendChild(buildNewMessage(message));
+};
 
 const buildNewMessage = (message) => {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(message))
-    return li;
-}
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(message));
+  return li;
+};
